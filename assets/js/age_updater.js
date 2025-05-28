7deda764572f020ceaa4659515a534980da04881
stngularity <stngularity@gmail.com>
2025-05-28 20:54:24 +0300

    fix format

--- assets/js/age_updater.js
+++ assets/js/age_updater.js
@@ -2,7 +2,8 @@ const birthday = new Date(document.getElementById("birthday").textContent),
       ageSpan = document.getElementById("age");
 
 function updateAge() {
-    ageSpan.textContent = Math.floor((new Date() - new Date(birthday).getTime()) / 3.15576e+10);
+    var bd = birthday.split(".");
+    ageSpan.textContent = Math.floor((new Date() - new Date(`${bd[1]}.${bd[0]}.${bd[2]}`).getTime()) / 3.15576e+10);
 }
 
 updateAge();
